0ddb9f61daaeb3a2f5256ba8286352dae1f927be
Donovan van Heerden <dp.vanheerden@gmail.com>
2016-11-25 06:23:03 +0200

    init

---
+++ app/redux-example.jsx
@@ -0,0 +1,9 @@
+var redux = require('redux');
+
+console.log('Starting redux example');
+
+// Pure function
+// 1. Always return the same result, given the same input
+// 2. Does not depend on variables outside of the method scope
+// 3. Cannot have async requests, promises
+// 4. Not allowed to update parameters
